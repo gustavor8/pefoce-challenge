dd764c37cee87fe302ba869668bd9a856bd31dd7
Gustavo Rodrigues <gustavogrmc@gmail.com>
2025-09-05 19:43:22 -0300

    Initial commit
    Basic layout defined

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'Pefoce Challenge';
+}

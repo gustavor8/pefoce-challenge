34e20c6065368320a20e7c48f54e6ae6af81e282
Gustavo Rodrigues <gustavogrmc@gmail.com>
2025-09-07 21:53:09 -0300

    feat: Generate basic pages

---
+++ src/app/features/pages/erro-page/erro-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ErroPageComponent } from './erro-page.component';
+
+describe('ErroPageComponent', () => {
+  let component: ErroPageComponent;
+  let fixture: ComponentFixture<ErroPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ErroPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ErroPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
